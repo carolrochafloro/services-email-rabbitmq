b033d7cf44129cf5d0176cf1b29961ed55f718dc
Carolina Floro <carolinarochaf@hotmail.com>
2024-05-25 10:32:58 -0300

    Criado model de contato e view de sucesso (mensagem enviada)

--- FormContato/Models/Contact.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FormContato.Models;
-
-public class Contact
-{
-
-}
---
+++ FormContato/Models/ContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormContato.Models;
+
+public class ContactViewModel
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    public string? Name { get; set; }
+
+    [Required]
+    public string? Email { get; set; }
+
+    [Required]
+    public string? Mensagem { get; set; }
+}
