04960f4a99b061557f8fe7572f4bf382f5effd20
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-01 17:10:19 -0300

    Criado service SendEmail e integrado com o Sendgrid.

---
+++ Email/Services/SendEmail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dotenv.net;
+using SendGrid;
+using SendGrid.Helpers.Errors.Model;
+using SendGrid.Helpers.Mail;
+using SendGrid.Helpers.Mail.Model;
+
+namespace Email.Services;
+internal class SendEmail
+{
+    public SendEmail()
+    {
+        DotEnv.Load();
+    }
+    public async Task Send(string message)
+    {
+        if (message == null)
+        {
+            throw new ArgumentNullException("message not found");
+        }
+
+        var client = new SendGridClient(Environment.GetEnvironmentVariable("SENDGRID_API_KEY"));
+        var from = new EmailAddress(Environment.GetEnvironmentVariable("FROM_EMAIL"), "Carol Rocha");
+        var subject = "You have a new message";
+        var to = new EmailAddress(Environment.GetEnvironmentVariable("TO_EMAIL"));
+        var plainText = message;
+        var html = "<p>Teste</p>";
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainText, html);
+        var response = await client.SendEmailAsync(msg);
+
+    }
+}
