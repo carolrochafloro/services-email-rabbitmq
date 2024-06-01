0c1592c6df30bd006eccb2f768f816cdf8082532
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-01 17:11:31 -0300

    Pequenas alterações e ajustes

--- Email/Program.cs
+++ Email/Program.cs
@@ -1,7 +1,10 @@
 using Email;
+using dotenv.net;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+DotEnv.Load();
 
 var host = builder.Build();
 host.Run();
