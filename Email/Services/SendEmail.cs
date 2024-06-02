b5fb4c0e92184027abb67d53b64505acc4f1e434
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-02 15:14:23 -0300

    Incluída resposta em caso de sucesso e erro do método SendEmail, desserialização da mensagem recebida pelo RabbitMQ e modelo HTML do e-mail

--- Email/Services/SendEmail.cs
+++ Email/Services/SendEmail.cs
@@ -8,6 +8,7 @@ using SendGrid;
 using SendGrid.Helpers.Errors.Model;
 using SendGrid.Helpers.Mail;
 using SendGrid.Helpers.Mail.Model;
+using System.Text.Json;
 
 namespace Email.Services;
 internal class SendEmail
@@ -16,21 +17,36 @@ internal class SendEmail
     {
         DotEnv.Load();
     }
-    public async Task Send(string message)
+    public async Task<bool> Send(string message)
     {
         if (message == null)
         {
-            throw new ArgumentNullException("message not found");
+            throw new ArgumentNullException("Message not found");
         }
+
+        var messageObject = JsonSerializer.Deserialize<Dictionary<string, string>>(message);
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string htmlFilePath = Path.Combine(currentDirectory, "Models", "Email.html");
 
         var client = new SendGridClient(Environment.GetEnvironmentVariable("SENDGRID_API_KEY"));
         var from = new EmailAddress(Environment.GetEnvironmentVariable("FROM_EMAIL"), "Carol Rocha");
         var subject = "You have a new message";
         var to = new EmailAddress(Environment.GetEnvironmentVariable("TO_EMAIL"));
         var plainText = message;
-        var html = "<p>Teste</p>";
+        var htmlTemplate = await File.ReadAllTextAsync(htmlFilePath);
+        var html = string.Format(htmlTemplate, messageObject["Nome"], messageObject["Email"], messageObject["Mensagem"]);
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainText, html);
         var response = await client.SendEmailAsync(msg);
 
+        if (response.IsSuccessStatusCode)
+        {
+            await Console.Out.WriteLineAsync($"E-mail successfully sent: {response.StatusCode}");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"Error: {response.StatusCode}");
+            return false;
+        }
     }
 }
