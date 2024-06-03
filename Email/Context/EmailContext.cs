b218a27d4926e83843d79cb21c521e995e97b3bd
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-03 13:07:59 -0300

    Integração do worker com SQL Server

---
+++ Email/Context/EmailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email.Context;
+internal class EmailContext : DbContext
+{
+    public EmailContext(DbContextOptions<EmailContext> options) : base(options)
+    {
+
+    }
+}
--- Email/Program.cs
+++ Email/Program.cs
@@ -1,10 +1,18 @@
 using Email;
 using dotenv.net;
+using Microsoft.EntityFrameworkCore;
+using Email.Context;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
 DotEnv.Load();
 
+string? connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+builder.Services.AddDbContext<EmailContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 var host = builder.Build();
 host.Run();
