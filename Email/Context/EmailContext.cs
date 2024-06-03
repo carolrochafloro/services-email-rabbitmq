9c221d8dd30af2dc3e5f2380ad42ae897fed2bde
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-03 13:59:33 -0300

    Incluído ContactViewModel no DBSet em EmailContext

--- Email/Context/EmailContext.cs
+++ Email/Context/EmailContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FormContato.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,10 +7,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Email.Context;
-internal class EmailContext : DbContext
+public class EmailContext : DbContext
 {
     public EmailContext(DbContextOptions<EmailContext> options) : base(options)
     {
 
     }
+    public DbSet<ContactViewModel> Contacts { get; set; }
+
 }
