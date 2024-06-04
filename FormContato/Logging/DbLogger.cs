3e54c34c4bd5357b7fd4592e7ec11eae2ca1a1ab
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-04 13:56:07 -0300

    Alterado log level para warning ou superior.

--- FormContato/Logging/DbLogger.cs
+++ FormContato/Logging/DbLogger.cs
@@ -20,7 +20,7 @@ public class DbLogger : ILogger
 
     public bool IsEnabled(LogLevel logLevel)
     {
-        return true;
+        return logLevel >= LogLevel.Warning;
     }
 
     public async void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
