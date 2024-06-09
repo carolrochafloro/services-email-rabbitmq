06ed09054be11da5a9cf33db7200b230ecce115a
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-09 12:44:08 -0300

    Criado automapper para DTOs e models

---
+++ FormContato/DTOs/Mapping/AutoMapping.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using FormContato.Models;
+
+namespace FormContato.DTOs.Mapping;
+
+public class AutoMapping : Profile
+{
+    public AutoMapping()
+    {
+        CreateMap<UserModel, RegisterDTO>().ReverseMap();
+    }
+}
