8dc66204e5e316adeb3ea2fe051968019767a51b
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-09 17:25:11 -0300

    Criado controller e página de login (falta gerenciamento de sessão, JWT e retornar a view correta)

---
+++ FormContato/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using FormContato.DTOs;
+using FormContato.Repositories;
+using FormContato.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormContato.Controllers;
+public class LoginController : Controller
+
+    // criar service para gerar JWT e refresh token e gerenciar sessão, criar model p/ sessão.
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public LoginController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View("Login");
+    }
+
+    [HttpPost]
+    public ActionResult Login(LoginDTO login)
+    {
+        if (login is null)
+        {
+            return BadRequest("All the data must be provided.");
+        }
+        try
+        {
+            var user = _unitOfWork.UserRepository.Get(u => u.Email == login.Email);
+
+            if (user is null)
+            {
+                return BadRequest("User not found.");
+            }
+
+            var hasher = new PasswordHasher();
+
+            bool isValidPassword = hasher.ComparePassword(login.Password, user.Salt, user.Password);
+
+            if (!isValidPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Incorrect user or password.");
+                return View("Login", login);
+            }
+
+            return View("Login"); // retornar página de usuário logado
+
+        } catch (Exception)
+        {
+            return View("Error");
+        }
+    }
+}
