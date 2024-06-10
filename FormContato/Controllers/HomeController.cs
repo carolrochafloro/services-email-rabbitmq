e8e7de183d88c03ed8de8869e5204b08682537ac
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-10 13:55:18 -0300

    Formulário de contato removido da Home

--- FormContato/Controllers/HomeController.cs
+++ FormContato/Controllers/HomeController.cs
@@ -9,46 +9,11 @@ using System.Diagnostics;
 
 namespace FormContato.Controllers;
 
-// ideia: tirar o form de home e só permitir o acesso a ele com url personalizada com infos do usuário cadastado
 public class HomeController : Controller
 {
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly IMapper _mapper;
-
-    public HomeController(IUnitOfWork unitOfWork, IMapper mapper)
-    {
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
-    }
-
     public IActionResult Index()
     {
         return View();
-    }
-
-    public async Task<IActionResult> SaveMessage(ContactDTO contact)
-    {
-        if (contact is null)
-        {
-            return View("Error");
-        }
-
-        var newContact = _mapper.Map<ContactModel>(contact);
-
-        try
-        {
-
-            _unitOfWork.ContactRepository.Create(newContact);
-            await _unitOfWork.CommitAsync();
-            var producer = new Producer();
-            producer.Produce(newContact);
-            return View("Success");
-        }
-        catch (Exception)
-        {
-            return View("Error");
-        }
-
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
