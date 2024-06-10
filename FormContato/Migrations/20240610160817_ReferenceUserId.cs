7f1711ab86fc1cc048641a27fc3e62f21f419992
Carolina Floro <carolinarochaf@hotmail.com>
2024-06-10 13:30:25 -0300

    Migration incluindo UserId como FK em Contacts

---
+++ FormContato/Migrations/20240610160817_ReferenceUserId.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormContato.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReferenceUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Contacts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "ContactDTO",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mensagem = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactDTO", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Users_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "ContactDTO");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Contacts");
+        }
+    }
+}
